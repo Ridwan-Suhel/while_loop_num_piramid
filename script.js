84d0f60b60ed2aa81d3d745294c68239e8fcd113
Ridwan Suhel <65410320+Ridwan-Suhel@users.noreply.github.com>
2021-08-14 00:39:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var n = 20;
+for(var i = 1; i <= n; i++){
+    result = " " + "<br>";
+    for (var j = 1; j <= i; j++){
+        result += j + " ";
+    }
+    document.write(result);
+}
+
+
+
+document.write("<br/><br/>");
+
+
+// var isRunning = true;
+// while(isRunning){
+//     var rand = Math.floor(Math.random() * 10 + 1);
+//     if(rand === 9){
+//         document.write("Winner winner");
+//         break;
+//     }else{
+//         document.write("You have got " + rand + "<br>");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
